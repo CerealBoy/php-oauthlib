70cb4c4805956d3917161c3c270165acbbe5fd74
CerealBoy <allan.shone@yahoo.com>
2013-02-04 22:15:41 +1100

    Cleaning some doccomments

--- src/OAuthLib/OAuth.php
+++ src/OAuthLib/OAuth.php
@@ -2,9 +2,8 @@
 /**
  * Main OAuth class.
  *
- * @namespace OAuthLib
- * @package php-oauthlib
  * @author Allan Shone <allan.shone@yahoo.com>
+ * @date Feb 2, 2013
  */
 
 namespace OAuthLib;
@@ -15,6 +14,9 @@ namespace OAuthLib;
  * This class provides all access and usage for OAuth.
  *   Whilst there are other classes provided within the OAuthLib namespace,
  *   this is the gateway and interfacing class.
+ *
+ * @package php-oauthlib
+ * @namespace OAuthLib
  */
 class OAuth
 {
@@ -32,11 +34,11 @@ class OAuth
      * Data for state and setting.
      *
      * <pre>
-     *   - <b>hash-algorithm</b> algorithm used for hashing
-     *   - <b>indexing</b> main cookie index
-     *   - <b>indexer</b> temp cookie index
-     *   - <b>salt</b> salt for use with hashing
-     *   - <b>cookie</b> current content of the cookie
+     *  <b>hash-algorithm</b> algorithm used for hashing
+     *  <b>indexing</b> main cookie index
+     *  <b>indexer</b> temp cookie index
+     *  <b>salt</b> salt for use with hashing
+     *  <b>cookie</b> current content of the cookie
      * </pre>
      *
      * @var array
