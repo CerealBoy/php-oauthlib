508e23733a2aeead2953caa3bf4a43f371c58202
CerealBoy <allan.shone@yahoo.com>
2013-02-17 23:22:02 +1100

    Filling in better test content

--- build/unit-tests/OAuthTest.php
+++ build/unit-tests/OAuthTest.php
@@ -1,11 +1,24 @@
 <?php
 
+require_once __DIR__ . '/../../autoload.php';
+
 class OAuthTest extends PHPUnit_Framework_TestCase
 {
     public function testConstruct()
     {
-        $result = true;
-        $this->assertTrue($result);
+        $lib = new OAuthLib\OAuth();
+        $this->assertInstanceOf('OAuthLib\OAuth', $lib);
+    }
+
+    public function testSignatureGenerate()
+    {
+        $lib = new OAuthLib\OAuth();
+        $lib->context(array(
+            'provider' => 'twitter',
+        ));
+        $signature = $lib->signature();
+
+        $this->assertEquals('replace-this-string', $signature);
     }
 }
 
