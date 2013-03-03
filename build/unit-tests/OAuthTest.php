16c6c2143d5d1a198abccff4fe3c79ce9abebc18
CerealBoy <allan.shone@yahoo.com>
2013-03-03 23:36:59 +1100

    Updating tests, adding several more

--- build/unit-tests/OAuthTest.php
+++ build/unit-tests/OAuthTest.php
@@ -15,10 +15,56 @@ class OAuthTest extends PHPUnit_Framework_TestCase
         $lib = new OAuthLib\OAuth();
         $lib->context(array(
             'provider' => 'twitter',
+            'hash-algorithm' => 'whirlpool',
+            'indexing' => 'va',
+            'id' => 'something',
         ));
         $signature = $lib->signature();
 
         $this->assertEquals('MzJjZWVjOGE2YzJhZGYxMDdlZmFmOWU2ODlmZjVlOWQzZmNmYWE3MDBjODA5YTc4ODUzYWFiMTJjNDFhNjJmZDIwZTFkNGNhYjJmMDJiYzcxMTQ0YThhYTQwZWE0OWNmZjQ4MGY5MThlMGJjZjc0YzUzM2VmZWU0MzY3NGUwODA=', $signature);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testContextArray()
+    {
+        $lib = new OAuthLib\OAuth();
+        $lib->context('twitter');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testContextProvider()
+    {
+        $lib = new OAuthLib\OAuth();
+        $lib->context(array('herp' => 'derp'));
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testContextNaming()
+    {
+        $lib = new OAuthLib\OAuth();
+        $lib->context(array(
+            'provider' => 'yahoo',
+            'herp' => 'derp',
+        ));
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testCookie()
+    {
+        $lib = new OAuthLib\OAuth();
+
+        $lib->context(array(
+            'provider' => 'facebook',
+        ));
+        $success = $lib->setCookie();
     }
 }
 
